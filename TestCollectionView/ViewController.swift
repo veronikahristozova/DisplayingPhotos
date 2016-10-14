264a4d369a5e45b9639f0fc51250914d6bab800b
Veronika Hristozova <veronikahristozova@192.168.0.107>
2016-10-14 18:02:10 +0300

    Displaying photos from camera roll

---
+++ TestCollectionView/ViewController.swift
@@ -0,0 +1,73 @@
+//
+//  ViewController.swift
+//  TestCollectionView
+//
+//  Created by Veronika Hristozova on 10/14/16.
+//  Copyright © 2016 Veronika Hristozova. All rights reserved.
+//
+
+import UIKit
+import Photos
+
+class ViewController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
+
+    var imageArray = [UIImage]()
+    
+    override func viewDidLoad() {
+        grabPhotos()
+    }
+    
+    func grabPhotos() {
+        let imgManager = PHImageManager.default()
+        let requestOptions = PHImageRequestOptions()
+        requestOptions.isSynchronous = true
+        requestOptions.deliveryMode = .highQualityFormat
+        
+        let fetchOptions = PHFetchOptions()
+        fetchOptions.sortDescriptors = [NSSortDescriptor(key: "creationDate", ascending: false)]
+        
+        let fetchResult = PHAsset.fetchAssets(with: .image, options: fetchOptions)
+            if fetchResult.count > 0 {
+                for i in 0..<fetchResult.count {
+                    imgManager.requestImage(for: fetchResult.object(at: i), targetSize: CGSize(width: 200, height: 200), contentMode: .aspectFill, options: requestOptions, resultHandler: { image, error in
+                        self.imageArray.append(image!)
+                        
+                    })
+                }
+            } else {
+                print("You do not have any photos taken!")
+                self.collectionView?.reloadData()
+            }
+        
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return imageArray.count
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath)
+        
+        let imageView = cell.viewWithTag(1) as! UIImageView
+        imageView.image = imageArray[indexPath.row]
+        
+        
+        return cell
+    }
+
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let width = collectionView.frame.width / 3 - 1
+        
+        return CGSize(width: width, height: width)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 1.0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 1.0
+    }
+}
+
